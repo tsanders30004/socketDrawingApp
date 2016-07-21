727ea6b850e9dab2c94566d1112105e584a29a79
Tim Sanders <tsanders30004@gmail.com>
2016-07-21 14:33:48 -0400

    21 Jul 2016:  The clear function is now working on both screens.

--- app.js
+++ app.js
@@ -3,15 +3,29 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-app.get('/', function(req, res){
-     res.sendfile('index.html');
-});
+app.use(express.static('./public'));
 
 io.on('connection', function(clientSocket) {
      clientSocket.on('draw', function(coords) {
           io.emit('draw', coords);
+
+          // clientSocket.on('clear', function() {
+          //      io.emit('clear');
+          // });
+     });
+
+     clientSocket.on('clear', function() {
+          console.log('xxx');
+          io.emit('clear');
      });
 });
+
+
+// io.on('connection', function(clientSocket) {
+//      clientSocket.on('clear', function() {
+//           io.emit('clear');
+//      });
+// });
 
 //create listener and broadcaster
 server.listen(3000, function() {
